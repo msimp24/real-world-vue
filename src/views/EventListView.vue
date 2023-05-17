8a8585c2f6a3700f1826634612e22b41b82c951a
Mark Simpson <mark.simpson4@gmail.com>
2023-05-17 20:59:17 -0300

    test

---
+++ src/views/EventListView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+import EventService from '../services/EventService.js'
+
+const events = ref(null)
+
+onMounted(async () => {
+  const response = await EventService.getEvents()
+  events.value = response.data
+})
+</script>
+
+<template>
+  <div class="events">
+    <h1>Events for Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
